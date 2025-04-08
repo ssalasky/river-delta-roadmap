616d5bba14d66ac90a56ecf9a50e61c1c4af1c33
samsalasky <samsalasky@gmail.com>
2025-04-07 21:21:54 -0600

    main: minor layout and data updates

--- src/app/[section]/page.tsx
+++ src/app/[section]/page.tsx
@@ -2,8 +2,9 @@ import DeployList from "@/components/DeployList";
 import MainColumn from "@/components/MainColumn";
 import SectionHeader from "@/components/SectionHeader";
 import Sidebar from "@/components/Sidebar";
-import {appData} from "@/data/data";
-import {FeatureStatuses} from '@/types/Feature';
+import SideColumn from "@/components/SideColumn";
+import { appData } from "@/data/data";
+import { FeatureStatuses } from "@/types/Feature";
 
 const SectionPage = async ({
   params,
@@ -11,24 +12,30 @@ const SectionPage = async ({
   params: Promise<{ section: keyof typeof appData }>;
 }) => {
   const { section } = await params;
-  const data = appData[section].filter((item) => item.status !== FeatureStatuses.Idea)
-  const ideaData = appData[section].filter((item) => item.status === FeatureStatuses.Idea)
+  const data = appData[section].filter(
+    (item) => item.status !== FeatureStatuses.Idea,
+  );
+  const ideaData = appData[section].filter(
+    (item) => item.status === FeatureStatuses.Idea,
+  );
 
   return (
     <>
-      <Sidebar/>
+      <Sidebar />
       <MainColumn>
-        <SectionHeader section={section}/>
-        <DeployList data={data}/>
+        <SectionHeader section={section} />
+        <DeployList data={data} />
       </MainColumn>
-      <aside
-        className="fixed top-16 inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
-        <ul role="list" className="divide-y divide-white/5">
+      <SideColumn>
+        <h2 className="font-semibold mb-2 border-b">
+          Future ideas under consideration:
+        </h2>
+        <ul role="list" className="divide-y divide-white/5 py-2 space-y-2">
           {ideaData.map((item) => (
             <p key={item.id}>{item.feature}</p>
           ))}
         </ul>
-      </aside>
+      </SideColumn>
     </>
   );
 };
--- src/components/SideColumn.tsx
+++ src/components/SideColumn.tsx
@@ -1,56 +1,11 @@
-import DeployListItem from '@/components/DeployListItem';
-import {DeploymentObject} from '@/types/Deployment';
+import { ReactNode } from "react";
 
-const deployments: DeploymentObject[] = [
-  {
-    id: 1,
-    projectName: 'ios-app',
-    teamName: 'Planetaria',
-    status: 'offline',
-    statusText: 'Initiated 1m 32s ago',
-    description: 'Deploys from GitHub',
-    environment: 'Preview',
-  },
-  {
-    id: 2,
-    projectName: 'mobile-api',
-    teamName: 'Planetaria',
-    status: 'online',
-    statusText: 'Deployed 3m ago',
-    description: 'Deploys from GitHub',
-    environment: 'Production',
-  },
-  {
-    id: 3,
-    projectName: 'tailwindcss.com',
-    teamName: 'Tailwind Labs',
-    status: 'offline',
-    statusText: 'Deployed 3h ago',
-    description: 'Deploys from GitHub',
-    environment: 'Preview',
-  },
-  {
-    id: 4,
-    projectName: 'api.protocol.chat',
-    teamName: 'Protocol',
-    status: 'error',
-    statusText: 'Failed to deploy 6d ago',
-    description: 'Deploys from GitHub',
-    environment: 'Preview',
-  },
-]
-
-const SideColumn = () => {
+const SideColumn = ({ children }: { children: ReactNode }) => {
   return (
-    <aside
-      className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
-      <ul role="list" className="divide-y divide-white/5">
-        {deployments.map((item) => (
-          <DeployListItem key={item.id} deployment={item} />
-        ))}
-      </ul>
+    <aside className="fixed mt-16 inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 sm:px-6 lg:px-8 xl:block">
+      {children}
     </aside>
-  )
-}
+  );
+};
 
-export default SideColumn
+export default SideColumn;
